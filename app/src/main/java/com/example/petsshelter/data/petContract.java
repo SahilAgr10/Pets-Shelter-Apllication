1d105ad3477b5a679b8fb70aa9c11324bbe68df7
Sahil Agrawal <agrawalsahil4994@gmail.com>
2022-09-24 14:00:16 +0530

    COntent Providers Added

--- app/src/main/java/com/example/petsshelter/data/petContract.java
+++ app/src/main/java/com/example/petsshelter/data/petContract.java
@@ -1,5 +1,6 @@
 package com.example.petsshelter.data;
 
+import android.net.Uri;
 import android.provider.BaseColumns;
 // contract class helps us to document the stucture of database (Schema).
 // Your contract basically defines your database and how people should interact with it through the Content Provider.
@@ -8,11 +9,19 @@ public final class petContract {
     // give it an empty constructor.
     private void PetContract() {}
 
+    public static final String CONTENT_AUTHORITY = "com.example.android.pets";
+    public static final Uri BASE_CONTENT_URI = Uri.parse("content://" + CONTENT_AUTHORITY);
+    public static final String PATH_PETS = "pets";
+
     /**
      * Inner class that defines constant values for the pets database table.
      * Each entry in the table represents a single pet.
      */
     public static final class PetEntry implements BaseColumns {
+
+
+        public static final Uri CONTENT_URI = Uri.withAppendedPath(BASE_CONTENT_URI, PATH_PETS);
+
 
         /** Name of database table for pets */
         public final static String TABLE_NAME = "pets";
@@ -21,6 +30,8 @@ public final class petContract {
         public final static String COLUMN_PET_BREED = "breed";
         public final static String COLUMN_PET_GENDER = "gender";
         public final static String COLUMN_PET_WEIGHT = "weight";
+
+
         //  Possible values for the gender of the pet.
         public static final int GENDER_UNKNOWN = 0;
         public static final int GENDER_MALE = 1;
